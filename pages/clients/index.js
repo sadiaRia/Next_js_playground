37b1d2d20641c9c00c77d3b32ddc006589698634
sadia <sadia@techhivebd.com>
2023-10-26 11:35:25 +0600

    add dynamuc route

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 function clientpage(props) {
+  let clients = [
+    { id: "01", name: "harry potter" },
+    { id: "02", name: "Ron wisly" },
+    { id: "03", name: "Hemaini grenger" },
+  ];
   return (
     <div>
       <h1>from clients main page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
